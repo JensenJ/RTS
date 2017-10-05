d4544b07ec53d806f496cf4672f741b7a20ed471
JensenJ <jensenbradley@icloud.com>
2017-10-05 19:32:36 +0100

    Camera Scrolling Added

--- TileBasedRTS/Assets/CameraController.cs
+++ TileBasedRTS/Assets/CameraController.cs
@@ -7,6 +7,9 @@ public class CameraController : MonoBehaviour {
     public float panSpeed = 20f;
     public float panBorderThickness = 10f;
     public Vector2 panLimit;
+    public float scrollSpeed = 20f;
+    public float minY = 20f;
+    public float maxY = 80f;
 
 	void Update () {
 
@@ -27,7 +30,11 @@ public class CameraController : MonoBehaviour {
         {
             pos.x -= panSpeed * Time.deltaTime;
         }
+
+       float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= scroll * scrollSpeed * 100 * Time.deltaTime;
         pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
         pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
 
         transform.position = pos;
