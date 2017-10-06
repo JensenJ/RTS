7271acd8b8596149ceaa033d98c64c78d9380629
JensenJ <jensenbradley@icloud.com>
2017-10-06 22:34:55 +0100

    Simple UI with minimap, resources

--- TileBasedRTS/Assets/CameraController.cs
+++ TileBasedRTS/Assets/CameraController.cs
@@ -5,33 +5,36 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     public float panSpeed = 20f;
-    public float panBorderThickness = 10f;
     public Vector2 panLimit;
     public float scrollSpeed = 20f;
-    public float minY = 20f;
-    public float maxY = 80f;
+    public float minY = 10f;
+    public float maxY = 40f;
 
 	void Update () {
 
         Vector3 pos = transform.position;
-        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        if (Input.GetKey("w"))
         {
             pos.z += panSpeed * Time.deltaTime;
+            pos.x -= panSpeed * Time.deltaTime;
         }
-        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        if (Input.GetKey("s"))
         {
             pos.z -= panSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
-        {
             pos.x += panSpeed * Time.deltaTime;
         }
-        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        if (Input.GetKey("d"))
         {
+            pos.z += panSpeed * Time.deltaTime;
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.z -= panSpeed * Time.deltaTime;
             pos.x -= panSpeed * Time.deltaTime;
         }
 
-       float scroll = Input.GetAxis("Mouse ScrollWheel");
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
         pos.y -= scroll * scrollSpeed * 100 * Time.deltaTime;
         pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
         pos.y = Mathf.Clamp(pos.y, minY, maxY);
