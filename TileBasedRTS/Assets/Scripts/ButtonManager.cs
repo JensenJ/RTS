b763e49e65362201ba963d6e33060ec3b08c5ae1
JensenJ <jensenbradley@icloud.com>
2017-10-08 15:02:14 +0100

    UI Scaleable and Lighting Rebuilt

---
+++ TileBasedRTS/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    public void ChangeToScene(int sceneToChangeTo)
+    {
+        SceneManager.LoadScene(sceneToChangeTo);
+    }
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
